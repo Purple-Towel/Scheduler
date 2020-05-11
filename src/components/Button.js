1a9dae051c7f05c1fb62a612af2460c70bf3aca0
Angad <angad@Angads-MBP.lan>
2020-05-11 15:44:04 -0700

    completed button component to satisfy button story in storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 import "components/Button.scss";
+import Classnames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+   let classNames = Classnames(buttonClass, {"button--confirm": props.confirm}, {"button--danger": props.danger})
+
+   return (
+      <button 
+         className={classNames} 
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
