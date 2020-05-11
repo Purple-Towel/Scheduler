9f8174bbee53e30ae720ac4916a84c22495876d9
Angad <angad@Angads-MBP.lan>
2020-05-11 16:28:31 -0700

    added unit tests for DayListItem and formatting function for spots remaining in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,13 +6,26 @@ import Classnames from "classnames";
 export default function DayListItem(props) {
   let dayClass = {"day-list__item": true, "day-list__item--selected": props.selected, "day-list__item--full": (props.spots === 0)};
   let classNames = Classnames(dayClass);
+
+  
+  const formatSpots = () => {
+    let { spots } = props;
+    if (spots === 0) {
+      return "no spots remaining"
+    } else if (spots === 1) {
+        return `${spots} spot remaining`
+    } else {
+      return `${spots} spots remaining`
+    }
+  };
+
   return (
     <li 
       className={classNames}
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
