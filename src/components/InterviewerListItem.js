4d9e918ed4d22fd545411e272f73ece56d5eb546
Angad <angad@Angads-MBP.lan>
2020-05-12 16:22:37 -0700

    added InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss"
+import Classnames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const { name, avatar, selected, setInterviewer} = props;
+  let interviewerClass = {"interviewers__item": true, "interviewers__item--selected": selected}
+  let classNames = Classnames(interviewerClass);
+
+
+return (
+    <li 
+      className={classNames}
+      onClick={setInterviewer}
+    >
+    <img
+      className="interviewers__item-image"
+      src={avatar}
+      alt={name}
+    />
+    {name}
+    </li>
+  );
+};
